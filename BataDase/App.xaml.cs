d464e953418aead453c4f1a43456470ebb63c141
Vladimir Nikulin <mail.jorey@gmail.com>
2021-05-15 20:48:29 +0300

    Added change of language and font.

--- BataDase/App.xaml.cs
+++ BataDase/App.xaml.cs
@@ -1,20 +1,114 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Windows;
+using System.Data;
 
 namespace BataDase
 {
 
     public partial class App : Application
     {
-        private const string URI_THEME_LIGHT = "/Resources/Palettes/LightPalette.xaml";
-        private const string URI_THEME_DARK = "/Resources/Palettes/DarkPalette.xaml";
+        private const string URI_THEME_LIGHT = "/Resources/Palettes/Palette.Light.xaml";
+        private const string URI_THEME_DARK = "/Resources/Palettes/Palette.xaml";
+
+        private static List<CultureInfo> m_Languages = new List<CultureInfo>();
+
+        public static List<CultureInfo> Languages
+        {
+            get
+            {
+                return m_Languages;
+            }
+        }
 
         private ResourceDictionary PaletteDictionary
         {
-            get { return Resources.MergedDictionaries[0]; }
+            get 
+            {
+                return (from d in Application.Current.Resources.MergedDictionaries
+                        where d.Source != null && d.Source.OriginalString.StartsWith("Resources/Palettes/Palette.")
+                        select d).First();
+            }
         }
 
         private bool IsDarkPalette = true;
+
+        public App()
+        {
+            InitializeComponent();
+            App.LanguageChanged += App_LanguageChanged;
+
+            m_Languages.Clear();
+            m_Languages.Add(new CultureInfo("ru-RU"));  // Нейтральная культура для этого проекта
+            m_Languages.Add(new CultureInfo("en-US"));
+            m_Languages.Add(new CultureInfo("ja-JP"));
+
+            Language = BataDase.Properties.Settings.Default.DefaultLanguage;
+        }
+
+        // Эвент для оповещения всех окон приложения
+        public static event EventHandler LanguageChanged;
+
+        public static CultureInfo Language
+        {
+            get
+            {
+                return System.Threading.Thread.CurrentThread.CurrentUICulture;
+            }
+            set
+            {
+                if (value == null) throw new ArgumentNullException("value");
+                if (value == System.Threading.Thread.CurrentThread.CurrentUICulture) return;
+
+                //1. Меняем язык приложения:
+                System.Threading.Thread.CurrentThread.CurrentUICulture = value;
+
+                //2. Создаём ResourceDictionary для новой культуры
+                ResourceDictionary dict = new ResourceDictionary();
+                switch (value.Name)
+                {
+                    case "en-US":
+                    case "ja-JP":
+                        dict.Source = new Uri(String.Format("Resources/Languages/Lang.{0}.xaml", value.Name), UriKind.Relative);
+                        break;
+                    default:
+                        dict.Source = new Uri("Resources/Languages/Lang.xaml", UriKind.Relative);
+                        break;
+                }
+
+                //3. Находим старую ResourceDictionary и удаляем его и добавляем новую ResourceDictionary
+                ResourceDictionary oldDict = (from d in Application.Current.Resources.MergedDictionaries
+                                              where d.Source != null && d.Source.OriginalString.StartsWith("Resources/Languages/Lang.")
+                                              select d).First();
+
+                if (oldDict != null)
+                {
+                    int ind = Application.Current.Resources.MergedDictionaries.IndexOf(oldDict);
+                    Application.Current.Resources.MergedDictionaries.Remove(oldDict);
+                    Application.Current.Resources.MergedDictionaries.Insert(ind, dict);
+                }
+                else
+                {
+                    Application.Current.Resources.MergedDictionaries.Add(dict);
+                }
+
+                //4. Вызываем евент для оповещения всех окон.
+                LanguageChanged(Application.Current, new EventArgs());
+            }
+        }
+
+        private void Application_LoadCompleted(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+            Language = BataDase.Properties.Settings.Default.DefaultLanguage;
+        }
+
+        private void App_LanguageChanged(Object sender, EventArgs e)
+        {
+            BataDase.Properties.Settings.Default.DefaultLanguage = Language;
+            BataDase.Properties.Settings.Default.Save();
+        }
 
         public void ChangePalette()
         {
--- BataDase/Properties/Settings.Designer.cs
+++ BataDase/Properties/Settings.Designer.cs
@@ -8,21 +8,30 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-
-namespace BataDase.Properties
-{
+namespace BataDase.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("ru-RU")]
+        public global::System.Globalization.CultureInfo DefaultLanguage {
+            get {
+                return ((global::System.Globalization.CultureInfo)(this["DefaultLanguage"]));
+            }
+            set {
+                this["DefaultLanguage"] = value;
             }
         }
     }
