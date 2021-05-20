43cb002522a04e23ea87264e40b6a2b631d32b48
Vladimir Nikulin <mail.jorey@gmail.com>
2021-05-20 13:32:28 +0300

    Added test content for DataGrid.

--- BataDase/MVVM/Views/UserV.xaml.cs
+++ BataDase/MVVM/Views/UserV.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
 using System.Windows.Controls;
 
 namespace BataDase.MVVM.Views
@@ -6,10 +6,26 @@ namespace BataDase.MVVM.Views
     public partial class UserV : UserControl
     {
         private bool isTrue;
+        private class Object
+        {
+            public int Number { get; set; }
+        }
 
         public UserV()
         {
             InitializeComponent();
+
+            List<Object> someList = new List<Object>();
+
+            for (int i = 0; i < 200; i++)
+            {
+                Object obj = new Object();
+                obj.Number = i;
+
+                someList.Add(obj);
+            }
+
+            DataGrid_Tables.ItemsSource = someList;
         }
 
         private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
