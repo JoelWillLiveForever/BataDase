07dcd27937f346d6747305b4c8ea3a2595283832
Vladimir Nikulin <mail.jorey@gmail.com>
2021-05-28 16:17:22 +0300

    Added state change for RadioButtons.

--- BataDase/MVVM/Views/MenuV.xaml.cs
+++ BataDase/MVVM/Views/MenuV.xaml.cs
@@ -7,6 +7,23 @@ namespace BataDase.MVVM.Views
         public MenuV()
         {
             InitializeComponent();
-        }		
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Button_Account.IsChecked = false;
+            Button_Carriages.IsChecked = false;
+            Button_Cities.IsChecked = false;
+            Button_Info.IsChecked = false;
+            Button_Locomotives.IsChecked = false;
+            Button_Routes.IsChecked = false;
+            Button_Schedules.IsChecked = false;
+            Button_Settings.IsChecked = false;
+            Button_Tickets.IsChecked = false;
+            Button_Trains.IsChecked = false;
+            Button_Users.IsChecked = false;
+
+            (sender as RadioButton).IsChecked = true;
+        }
     }
 }
